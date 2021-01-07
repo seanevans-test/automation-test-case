d1e48b40ecbc40bae67e8fbf68bef150e053828e
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-07 20:22:13 +0000

    added first test for the loginTests

--- src/test/java/login/LoginTests.java
+++ src/test/java/login/LoginTests.java
@@ -1,6 +1,22 @@
 package login;
 
 import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.SecureAreaPage;
+
+import static org.testng.AssertJUnit.assertTrue;
 
 public class LoginTests extends BaseTests {
+
+    @Test
+    public void testSuccessfulLogin(){
+        LoginPage loginPage = homepage.clickFormAuthentication();
+        loginPage.setUsername("tomsmith");
+        loginPage.setPassword("SuperSecretPassword!");
+        SecureAreaPage secureAreaPage = loginPage.ClickLoginButton();
+        assertTrue("Alert text is incorrect",
+                secureAreaPage.getAlertText()
+                                .contains("You logged into a secure area! x"));
+    }
 }
