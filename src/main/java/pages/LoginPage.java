97d616a57b6b2ab098bd1a8cc943f5ab055cbe9e
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-07 19:03:44 +0000

    set variables for the LoginPage fields

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,10 +1,14 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
 
     private WebDriver driver;
+    private By usernameField = By.id("username");
+    private By passwordField = By.id("password");
+    private By loginButton = By.cssSelector("#login button");
 
     public LoginPage(WebDriver driver){
         this.driver = driver;
