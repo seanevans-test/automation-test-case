a434a212a7beba35215106bb9aafd3f4ed22fd34
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-07 19:17:41 +0000

    first commit on InfiniteScrollPage

---
+++ src/main/java/pages/InfiniteScrollPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class InfiniteScrollPage {
+
+    private WebDriver driver;
+
+    public InfiniteScrollPage(WebDriver driver){
+        this.driver = driver;
+    }
+}
