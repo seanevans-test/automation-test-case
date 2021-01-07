90f31bc36acb398eabe8a773bb45be723f26805b
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-07 19:41:12 +0000

    set up the basis of the test with testNG

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,10 +1,28 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import pages.Homepage;
 
 public class BaseTests {
 
-    private WebDriver driver;
+    private EventFiringWebDriver driver;
     protected Homepage homepage;
+
+    @BeforeClass
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        goHome();
+
+        homepage = new Homepage(driver);
+    }
+
+    @BeforeMethod
+    public void goHome(){
+        driver.get("https://the-internet.herokuapp.com/");
+    }
 }
