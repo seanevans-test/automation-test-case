02b375dfc320e36b52f0fcbbc1b3a34c44c2c34a
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-07 18:56:27 +0000

    added links to required testing pages onto the homepage

--- src/main/java/pages/Homepage.java
+++ src/main/java/pages/Homepage.java
@@ -8,7 +8,23 @@ public class Homepage {
     private WebDriver driver;
 
     public Homepage(WebDriver driver){
-        this.driver = driver};
+        this.driver = driver;
+    }
+
+    public LoginPage clickFormAuthentication(){
+        clickLink("Form Authentication");
+        return new LoginPage(driver);
+    }
+
+    public InfiniteScrollPage clickInfiniteScrollPage(){
+        clickLink("Infinite Scroll");
+        return new InfiniteScrollPage(driver);
+    }
+
+    public KeyPressesPage clickKeyPresses(){
+        clickLink("Key Presses");
+        return new KeyPressesPage(driver);
+    }
 
     private void clickLink(String linkText){
         driver.findElement(By.linkText(linkText)).click();
