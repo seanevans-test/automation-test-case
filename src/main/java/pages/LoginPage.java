3087c7b02379cfb6245718d019dd82cc1a4e154d
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-07 19:08:59 +0000

    added findelements and interaction options

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -13,4 +13,17 @@ public class LoginPage {
     public LoginPage(WebDriver driver){
         this.driver = driver;
     }
+
+    public void setUsername(String username){
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void setPassword(String password){
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public SecureAreaPage ClickLoginButton(){
+        driver.findElement(loginButton).click();
+        return new SecureAreaPage(driver);
+    }
 }
