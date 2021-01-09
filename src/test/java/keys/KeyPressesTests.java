d55dea305224f442ef372519530c80c2489e1c5a
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-09 13:38:15 +0000

    created test for first KeyPress

--- src/test/java/keys/KeyPressesTests.java
+++ src/test/java/keys/KeyPressesTests.java
@@ -1,6 +1,18 @@
 package keys;
 
 import base.BaseTests;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import pages.KeyPressesPage;
+
+import static org.testng.Assert.assertEquals;
 
 public class KeyPressesTests extends BaseTests {
+
+    @Test
+    public void testBackSpace(){
+        KeyPressesPage keyPressesPage = homepage.clickKeyPresses();
+        keyPressesPage.enterText("A" + Keys.BACK_SPACE);
+        assertEquals(keyPressesPage.getResult(), "You entered: BACK_SPACE");
+    }
 }
