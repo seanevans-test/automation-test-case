d79b9dc90677b5dcf4d038a3e7760cb28d009395
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-10 16:17:42 +0000

    added test for scrolling down, down again, up to the top and asserting the header

--- src/test/java/scroll/ScrollTests.java
+++ src/test/java/scroll/ScrollTests.java
@@ -4,10 +4,16 @@ import base.BaseTests;
 import org.testng.annotations.Test;
 import pages.InfiniteScrollPage;
 
+import static org.testng.Assert.assertTrue;
+
 public class ScrollTests extends BaseTests {
 
     @Test
-    public void testScrollToBottomParagraph(){
-        homepage.clickInfiniteScrollPage().scrollToParagraph(5);
+    public void testInfiniteScroll(){
+        InfiniteScrollPage infiniteScrollPage = homepage.clickInfiniteScrollPage();
+        infiniteScrollPage.scrollToBottom(5);
+        infiniteScrollPage.scrollToBottom(9);
+        infiniteScrollPage.scrollToTop();
+        assertTrue(infiniteScrollPage.getHeaderText().contains("Infinite Scroll"));
     }
 }
