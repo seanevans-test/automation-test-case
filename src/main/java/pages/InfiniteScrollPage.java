25e1c6d298c6b246b2b70a70e8fb9a27c1bccb2a
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-10 12:40:20 +0000

    added scroll to paragraph method and while loop

--- src/main/java/pages/InfiniteScrollPage.java
+++ src/main/java/pages/InfiniteScrollPage.java
@@ -1,15 +1,34 @@
 package pages;
 
-import jdk.nashorn.internal.ir.CallNode;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public class InfiniteScrollPage {
 
     private WebDriver driver;
+    By textBlocks = By.className("jscroll-added");
 
     public InfiniteScrollPage(WebDriver driver){
         this.driver = driver;
+    }
+
+    /**
+     * Scrolls intil the paragraph with index specified is in view
+     * @param index
+     */
+
+    public void scrollToParagraph(int index){
+        String script = "window.scrollTo(0, document.body.scrollHeight)";
+        var jsExecutor = (JavascriptExecutor)driver;
+
+        while (getNumberofParagraphsPresent() < index){
+            jsExecutor.executeScript(script);
+        }
+    }
+
+    private int getNumberofParagraphsPresent(){
+        return driver.findElements(textBlocks).size();
     }
 
 }
