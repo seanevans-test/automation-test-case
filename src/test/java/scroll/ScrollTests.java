cf102125bf276dee20966f8d76c4154b63307b70
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-10 12:53:31 +0000

    created first part of the test scrolling down

--- src/test/java/scroll/ScrollTests.java
+++ src/test/java/scroll/ScrollTests.java
@@ -1,6 +1,13 @@
 package scroll;
 
 import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.InfiniteScrollPage;
 
 public class ScrollTests extends BaseTests {
+
+    @Test
+    public void testScrollToBottomParagraph(){
+        homepage.clickInfiniteScrollPage().scrollToParagraph(5);
+    }
 }
