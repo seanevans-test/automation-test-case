fee809afdf6b7b5eea8b0517bd58659586133804
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-21 17:08:13 +0000

    first commit on ForgotPasswordPage

---
+++ src/main/java/pages/ForgotPasswordPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ForgotPasswordPage {
+
+    private WebDriver driver;
+
+    public ForgotPasswordPage(WebDriver driver){this.driver = driver;}
+}
