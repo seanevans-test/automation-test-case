18a30a070b5f6547e8fa7c8d4dbaa49586e2d866
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-21 17:19:37 +0000

    added email input

--- src/main/java/pages/ForgotPasswordPage.java
+++ src/main/java/pages/ForgotPasswordPage.java
@@ -1,10 +1,17 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ForgotPasswordPage {
 
     private WebDriver driver;
+    private By emailInputField = By.id("email");
+    private By retrievePasswordField = By.id("form_submit");
 
     public ForgotPasswordPage(WebDriver driver){this.driver = driver;}
+
+    public void setEmail(String email){driver.findElement(emailInputField).sendKeys(email);}
+
+
 }
