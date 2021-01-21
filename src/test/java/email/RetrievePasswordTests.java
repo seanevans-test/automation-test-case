de0281f14e87f0875f1be91af4543979d2fde330
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-21 17:57:42 +0000

    added test for the email retrieval

--- src/test/java/email/RetrievePasswordTests.java
+++ src/test/java/email/RetrievePasswordTests.java
@@ -1,6 +1,19 @@
 package email;
 
 import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.EmailSentPage;
+import pages.ForgotPasswordPage;
+
+import static org.testng.Assert.assertTrue;
 
 public class RetrievePasswordTests extends BaseTests {
+
+    @Test
+    public void testEmailRetrieval(){
+        ForgotPasswordPage forgotPasswordPage = homepage.clickForgotPassword();
+        forgotPasswordPage.setEmail("test@test.com");
+        EmailSentPage emailSentPage = forgotPasswordPage.clickRetrievePasswordButton();
+        assertTrue(emailSentPage.getSendCheck().contains("Your e-mail's been sent!"));
+    }
 }
