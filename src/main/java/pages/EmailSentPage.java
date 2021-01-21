a679641efe73057097c9c3053a61e1af733da656
seanevans-test <sean-.-evans@hotmail.co.uk>
2021-01-21 17:26:27 +0000

    added check for email sent text

--- src/main/java/pages/EmailSentPage.java
+++ src/main/java/pages/EmailSentPage.java
@@ -1,10 +1,14 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class EmailSentPage {
 
     private WebDriver driver;
+    private By sendCheck = By.id("content");
 
     public EmailSentPage(WebDriver driver){this.driver = driver;}
+
+    public String getSendCheck(){return driver.findElement(sendCheck).getText();}
 }
